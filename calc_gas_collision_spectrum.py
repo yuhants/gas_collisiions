ae70e1edaf3eeaecc13217ea518011902a89952c
Yu-Han Tseng <yuhan@yuhants.domain.com>
2026-02-12 15:18:21 -0500

    likelihood fit for sphere 20260105

--- calc_gas_collision_spectrum.py
+++ calc_gas_collision_spectrum.py
@@ -10,7 +10,7 @@ c = 299792458    # m / s
 SI2ev = (1 / 1.6e-19) * c
 ev2SI = 1 / (SI2ev)
 
-T = 293
+T_gas = 293
 
 def fmb(dp, mg, vbar):
     return np.exp(-1 * dp**2 / (8 * mg**2 * vbar**2)) / np.sqrt(2 * np.pi * vbar**2)
@@ -18,19 +18,32 @@ def fmb(dp, mg, vbar):
 def xi(x):
     return np.sqrt(np.pi) * x * (1 - 2/(x**2)) * erf(x/2) * np.exp(-x**2 / 8) + 2 * np.exp(-3 * x**2 / 8)
 
-def dgamma_dp(pp_kev, mg_amu, p_mbar, alpha, T=293, sphere_radius=83e-9):
+def dgamma_dp_specular(sphere_radius, pp_kev, mg_amu, p_mbar, T_gas):
     A = 4 * np.pi * (sphere_radius)**2  # Surface area of sphere (m^2)
 
     pp = pp_kev * 1000 * ev2SI
     mg = mg_amu * 1.660538921e-27
     p_pascal = p_mbar * 100
-    ng = p_pascal / (kb * T)
-    
-    vbar = np.sqrt(kb * T / mg)
-    
-    # rate = (ng * A * pp / (4 * mg**2)) * fmb(mg, pp/(2*mg), T) * (1 - alpha + alpha * xi(pp/(mg*vbar)))
-    rate = (ng * A * pp / (4 * mg**2)) * fmb(pp, mg, vbar) * (1 - alpha + alpha * xi(pp/(mg*vbar)))
-    rate_hz_kev = rate * 1000 * ev2SI  # Hz/keV
+
+    ng = p_pascal / (kb * T_gas)
+    vbar = np.sqrt(kb * T_gas / mg)
+
+    rate = (ng * A * pp / (4 * mg**2)) * fmb(pp, mg, vbar)
+    rate_hz_kev = rate * 1000 * ev2SI
+    return rate_hz_kev
+
+def dgamma_dp_diffuse(sphere_radius, pp_kev, mg_amu, p_mbar, T_gas):
+    A = 4 * np.pi * (sphere_radius)**2  # Surface area of sphere (m^2)
+
+    pp = pp_kev * 1000 * ev2SI
+    mg = mg_amu * 1.660538921e-27
+    p_pascal = p_mbar * 100
+
+    ng = p_pascal / (kb * T_gas)
+    vbar = np.sqrt(kb * T_gas / mg)
+
+    rate = (ng * A * pp / (4 * mg**2)) * fmb(pp, mg, vbar) * xi(pp/(mg*vbar))
+    rate_hz_kev = rate * 1000 * ev2SI
     return rate_hz_kev
 
 def zeta(a, c, d):
@@ -47,7 +60,7 @@ def zeta(a, c, d):
 
     return prefactor * ((first_0 + first_1) + second_pre * second) / denominator
     
-def dgamma_dp_diffuse(pp_kev, mg_amu, p_mbar, tl=293, th=300, sphere_radius=83e-9):
+def dgamma_dp_diffuse_noneq(sphere_radius, pp_kev, mg_amu, p_mbar, tl=293, th=300):
     A = 4 * np.pi * (sphere_radius)**2  # Surface area of sphere (m^2)
 
     pp = pp_kev * 1000 * ev2SI      # N s
@@ -55,8 +68,6 @@ def dgamma_dp_diffuse(pp_kev, mg_amu, p_mbar, tl=293, th=300, sphere_radius=83e-
     
     p_pascal = p_mbar * 100
     ng = p_pascal / (kb * tl)
-    # print(ng)
-    # print(ng * A)
 
     vl = np.sqrt(kb * tl / mg)
     vh = np.sqrt(kb * th / mg)
@@ -65,6 +76,18 @@ def dgamma_dp_diffuse(pp_kev, mg_amu, p_mbar, tl=293, th=300, sphere_radius=83e-
     rate_hz_kev = rate / (SI2ev / 1000)
     
     return rate_hz_kev
+
+def dgamma_dp_tot_fixedtemp(pp_kev, mg_amu, p_mbar, alpha, T_gas=293, sphere_radius=50e-9):
+    rate_specular = dgamma_dp_specular(sphere_radius, pp_kev, mg_amu, p_mbar, T_gas)
+    rate_diffuse  = dgamma_dp_diffuse(sphere_radius, pp_kev, mg_amu, p_mbar, T_gas)
+
+    return (1 - alpha) * rate_specular + alpha * rate_diffuse
+
+def dgamma_dp_tot_noneq(pp_kev, mg_amu, p_mbar, alpha, T_gas=293, T_sensor=900, sphere_radius=50e-9):
+    rate_specular = dgamma_dp_specular(sphere_radius, pp_kev, mg_amu, p_mbar, T_gas)
+    rate_diffuse_noneq = dgamma_dp_diffuse_noneq(sphere_radius, pp_kev, mg_amu, p_mbar, tl=T_gas, th=T_sensor)
+
+    return (1 - alpha) * rate_specular + alpha * rate_diffuse_noneq
 
 def get_drdqz(qq, drdq):
     drdq_iso = drdq / (4 * np.pi * qq**2)
@@ -102,3 +125,28 @@ def smear_drdqz_gauss(qq, drdqz, sigma_kev):
     ret = convolved[idx_start : idx_start + drdqz.size] / np.sum(gauss_kernel)
 
     return qq, ret
+
+if __name__ == '__main__':
+    sphere_radius = 50e-9
+    mg_amu = 131.293
+    p_mbar = 1e-8
+    T_gas = 293
+
+    alpha_list = np.linspace(0, 1, 100)
+    ts_list = np.linspace(294, 1000, 100)
+
+    bins = np.arange(0, 2000, 25)
+    qq_kev = 0.5 * (bins[1:] + bins[:-1])
+    # qq_kev = np.linspace(10, 2500, 200)
+    drdqz_all = np.empty(shape=(alpha_list.size, ts_list.size, qq_kev.size), dtype=np.float64)
+    for i, alpha in enumerate(alpha_list):
+        # print(i)
+        for j, ts in enumerate(ts_list):
+            drdq = dgamma_dp_tot_noneq(qq_kev, mg_amu, p_mbar, alpha, T_gas=T_gas, T_sensor=ts, sphere_radius=sphere_radius)
+            qq, drdqz = get_drdqz(qq_kev, drdq)
+
+            drdqz_all[i][j] = drdqz
+    
+    outdir = r'/Users/yuhan/work/nanospheres/gas_collisiions/data_processed/gas_signal'
+    np.savez(f'{outdir}/xe_signal_{sphere_radius}nm_{p_mbar}mbar.npz', pressure=p_mbar, alpha_list=alpha_list, ts_list=ts_list, qq_kev=qq_kev, drdqz=drdqz_all)
+
---
+++ gas_likelihood_fit.py
@@ -0,0 +1,133 @@
+import h5py
+import sys
+
+import numpy as np
+from scipy.special import erf
+from scipy.optimize import minimize
+
+import calc_gas_collision_spectrum as gas
+
+from multiprocessing import Pool
+
+sphere = 'sphere_20260105'
+length_search_window = 5e-5  # length of the search window (50 us)
+
+eff_coefs = [2.88150446e+02, 1.08156304e-02]
+eff_chi2  = 1
+fit_band = (250, 400)
+nll_offset = 0
+
+def func_eff(x, z, f):
+    return 0.5 * erf((x - z) * f) + 0.5
+
+def gaus(x, mu, sigma):
+    return (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-1 * (x - mu)**2 / (2 * sigma**2))
+
+def gaus_normalized(x, mu, sigma, fit_band):
+    lb, ub = fit_band[0], fit_band[1]
+    norm = 0.5 * (erf((ub - mu)/(np.sqrt(2)*sigma)) - erf((lb - mu)/(np.sqrt(2)*sigma)))
+
+    x = np.asarray(x)
+    if x.size == 1:
+        return gaus(x, mu, sigma)[0] / norm
+    else:
+        return gaus(x, mu, sigma) / norm
+
+def calc_nll(sigma, drdqzn, bc, hist, eff_coefs, eff_chi2, fit_band, nll_offset=0):
+    # Correct for signal efficiency (search and chi2 cut)
+    hist_norm = np.sum(hist) * length_search_window * (bc[1] - bc[0])
+
+    eff_qq = func_eff(bc, *eff_coefs)
+    hist_gas = eff_chi2 * eff_qq * drdqzn * hist_norm
+
+    idx_fit = np.logical_and(bc > fit_band[0], bc < fit_band[1])
+    bi = bc[idx_fit]
+    ni = hist[idx_fit]
+
+    gas_contribution = np.sum(hist_gas[idx_fit])
+    ntot = np.sum(ni) - gas_contribution
+
+    if ntot > 0:
+        # Use only the central value of pdf
+        # faster and avoid numerical issues from integration
+        # No correctiion for efficiency for background
+        joint_pdf = gaus_normalized(bi, 0, sigma, fit_band)
+        mui = ntot * joint_pdf * 50 + hist_gas[idx_fit]
+    else:
+        mui = hist_gas[idx_fit]
+    mui[ mui < 1e-30 ] = 1e-30  # set a very small value so the log doesn't overflow
+
+    return np.sum(np.nan_to_num(mui - ni * np.log(mui))) + nll_offset
+
+def nll_sigma_pressure(sigma, log10_pressure, drdqz, bc, hist, eff_coefs, eff_chi2, fit_band, nll_offset=0):
+    log10_pressure_ref = -8
+    pressure_scale = np.power(10, (log10_pressure - log10_pressure_ref))
+
+    _, drdqzn = gas.smear_drdqz_gauss(bc, drdqz, sigma)
+    drdqzn_scaled = drdqzn * pressure_scale
+    _nll = calc_nll(sigma, drdqzn_scaled, bc, hist, eff_coefs, eff_chi2, fit_band, nll_offset)
+
+    return _nll
+
+def minimize_nll(drdqz, bc, hist, eff_coefs, eff_chi2, fit_band, nll_offset):
+    bounds = [(25, 125), (-13, -5)]
+    args = [drdqz, bc, hist, eff_coefs, eff_chi2, fit_band, nll_offset]
+    res = minimize(fun=lambda x: nll_sigma_pressure(*x, *args), x0=[70, -7],
+                    method='Nelder-Mead',
+                    bounds=bounds,
+                    options={'disp' : False,
+                            'maxiter': 50000,
+                            'maxfev': 50000,
+                            'adaptive': True,
+                            'fatol': 0.001,
+                            }
+                    )
+    if res.success:
+        return res.fun, res.x
+    else:
+        return np.nan, [np.nan, np.nan]
+    
+if __name__ == '__main__':
+    # Read reconstruction data
+    sphere = 'sphere_20260105'
+    datasets_all = ['20260107_p8e_4e-8mbar', '20260107_p8e_3e-8mbar_valveclosed', 
+                    '20260107_p8e_5e-8mbar', '20260107_p8e_6e-8mbar', '20260107_p8e_8e-8mbar', 
+                    '20260107_p8e_1e-7mbar', '20260107_p8e_2e-7mbar']
+    outfile_name = f'{sphere}_gas_recon_all.h5py'
+    outdir = r'/Users/yuhan/work/nanospheres/gas_collisiions/data_processed/gas_recon'
+
+    with h5py.File(f'{outdir}/{outfile_name}', 'r') as f:
+        g = f['recon_histograms']
+        bc = g['bc'][:]
+        hh_0 = g[f'hist_{datasets_all[0]}'][:]
+        hh_1 = g[f'hist_{datasets_all[1]}'][:]
+        hh_2 = g[f'hist_{datasets_all[2]}'][:]
+        hh_3 = g[f'hist_{datasets_all[3]}'][:]
+        hh_4 = g[f'hist_{datasets_all[4]}'][:]
+        hh_5 = g[f'hist_{datasets_all[5]}'][:]
+        hh_6 = g[f'hist_{datasets_all[6]}'][:]
+
+    # Read signal file
+    signal_file = r'/Users/yuhan/work/nanospheres/gas_collisiions/data_processed/gas_signal/xe_signal_5e-08nm_1e-08mbar.npz'
+    with np.load(signal_file) as f:
+        alpha_list = f['alpha_list']
+        ts_list = f['ts_list']
+        qq_kev = f['qq_kev']
+        drdqzs = f['drdqz']
+
+    out_dir = r'/Users/yuhan/work/nanospheres/gas_collisiions/data_processed/likelihood_fit'
+    hhs_all = [hh_0, hh_1, hh_2, hh_3, hh_4, hh_5, hh_6]
+    for i, hist in enumerate(hhs_all):
+        outfile = f'{datasets_all[i]}_likelihood_fit.npz'
+
+        nlls_all = np.empty((alpha_list.size, ts_list.size))
+        params_all = np.empty((alpha_list.size, ts_list.size, 2))
+        for i in range(alpha_list.size):
+            for j in range(ts_list.size):
+                drdqz = drdqzs[i][j]
+                _nll, _params = minimize_nll(drdqz, bc, hist, eff_coefs, eff_chi2, fit_band, nll_offset)
+                nlls_all[i][j] = _nll
+                params_all[i][j] = _params
+
+        print(f'Writing file {out_dir}/{outfile}')
+        np.savez(f'{out_dir}/{outfile}', alpha_list=alpha_list, ts_list=ts_list, nlls_all=nlls_all, params_all=params_all)
